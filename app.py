de80d6599db448ac3cc5a591aa9fb2d979bb8f92
CIchyFlashy <mateusz.cichos@edu.uekat.pl>
2024-10-28 12:51:49 +0100

    Add functions and routes for subpages

--- app.py
+++ app.py
@@ -6,5 +6,33 @@ app = Flask(__name__)
 def index():
     return '<h1>Hello world!!!</h1>'
 
+@app.route('/kontakt')
+def contact():
+    return '<h1>Kontakt</h1>'
+
+@app.route('/o-nas')
+def about():
+    return '<h1>O nas</h1>'
+
+@app.route('/uslugi')
+def services():
+    return '<h1>Usługi</h1>'
+
+@app.route('/uslugi/naprawa')
+def fix():
+    return '<h1>Naprawa</h1>'
+
+@app.route('/uslugi/komis')
+def dealership():
+    return '<h1>Komis</h1>'
+
+@app.route('/uslugi/czesci')
+def parts():
+    return '<h1>Części samochodowe</h1>'
+
+@app.route('/uslugi/detailing')
+def detailing():
+    return '<h1>Detailing</h1>'
+
 if __name__ == "__main__":
     app.run()
