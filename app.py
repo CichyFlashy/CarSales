c2fab1c636d6358179193f770361bfdbcbd4261b
Mateusz Cichos <mateusz.cichos@edu.uekat.pl>
2025-04-05 00:19:07 +0200

    Update homepage

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, EmailField, PasswordField
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'WSXedc1234'
+
+class RegistrationForm(FlaskForm):
+    name = StringField('User name')
+    last_name = StringField('User last name')
+    email = EmailField('User e-mail')
+    password = PasswordField('User password')
+    
 
 @app.route('/')
 def index():
@@ -13,10 +23,6 @@ def contact():
 @app.route('/o-nas')
 def about():
     return render_template('about.html')
-
-# @app.route('/uslugi')
-# def services():
-#     return render_template('index.html')
 
 @app.route('/uslugi/naprawa')
 def fix():
@@ -33,6 +39,11 @@ def parts():
 @app.route('/uslugi/detailing')
 def detailing():
     return render_template('detailing.html')
+
+@app.route('/registration')
+def registration():
+    form = RegistrationForm()
+    return render_template('/registration.html', form=form)
 
 if __name__ == "__main__":
     app.run()
